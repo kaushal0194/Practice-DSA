e1e77964b9adf6bde1c607785c601a027c580e87
kaushal0194 <kaushalkhatri4356@gmail.com>
2026-08-25 14:08:48 +0530

    Time: 13 ms (6.39%), Space: 52.6 MB (6.12%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> st = new Stack<>();
+        String res="";
+        for (int i = 0; i < path.length(); i++) {
+            if (path.charAt(i) == '/') {
+                continue;
+            }
+            String temp = "";
+            while (i < path.length() && path.charAt(i) != '/') {
+                temp += path.charAt(i);
+                ++i;
+            }
+            if (temp.equals(".") ) {
+                continue;
+
+            } else if (temp.equals( "..")) {
+                if (!st.isEmpty()) {
+                    st.pop();
+                }
+            } 
+            else {
+                st.push(temp);
+            }
+
+        }
+        while (!st.isEmpty()) {
+            res = "/" + st.pop() + res;
+
+            
+
+        }
+        if(res.length()==0){
+            return "/";
+        }
+        return res;
+
+    }
+}
